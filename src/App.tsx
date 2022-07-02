422195c6fc62d59d8fb74a8ede6145c3e76d3f91
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-07-02 21:30:26 +0900

    Routesを設定。NotFoundまで。

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Router, Routes } from "react-router-dom";
+import "./App.css";
+import { About } from "./components/About";
+import { Contact } from "./components/Contact";
+import { Home } from "./components/Home";
+import { NotFound } from "./components/NotFound";
 
 function App() {
   return (
     <div className="App">
       <h1>React router v6</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
